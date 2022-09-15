6f2510190d37070e473640eadd8f4fb5d6d4ed89
Mateusz Pleśniak <mplesniak@sii.pl>
2022-09-15 13:39:24 +0200

    applied: Fix for loading image from file

--- BasicGUI/LedRuler.cpp
+++ BasicGUI/LedRuler.cpp
@@ -1,88 +1,68 @@
 
 #include "LedRuler.h"
 #include "Interpolation.h"
-#include <QDebug>
 
-LedRuler::LedRuler(QQuickItem *parent): QQuickPaintedItem(parent),  
-                                        m_pix_rect(0,0, 50,50)            
-{
+LedRuler::LedRuler(QQuickItem *parent)
+    : QQuickPaintedItem(parent), m_pix_rect(0,0, 50,50) {
     m_rotation =0;  
 }
 
+QColor LedRuler::color() const{ return m_color;}
 
-QColor LedRuler::color() const
-{
-    return m_color;
-}
+void LedRuler::setColor(const QColor &color){ m_color = color; }
 
-void LedRuler::setColor(const QColor &color)
-{
-    m_color = color;
-}
-
-void LedRuler::setSize( const int &size)
-{
+void LedRuler::setSize( const int &size){
     m_pix_rect.setHeight(size);
     m_pix_rect.setWidth(size);
 }
 
-int LedRuler::size() const
-{
-    return m_pix_rect.height();
+int LedRuler::size() const { return m_pix_rect.height(); }
+
+int LedRuler::lx() const { return m_pix_rect.x(); }
+
+int LedRuler::ly() const { return m_pix_rect.y(); }
+
+void LedRuler::setLx(const int &lx) { m_pix_rect.setX(lx); }
+
+void LedRuler::setLy(const int &ly) { m_pix_rect.setY(ly); }
+
+int LedRuler::rotation() const { return m_rotation; }
+
+void LedRuler::setRotation(const int &rotation) { m_rotation = rotation; }
+
+QPixmap LedRuler::getPixMap() const { return m_part_map; }
+
+bool LedRuler::setPixMap(QUrl path){
+  
+  if (!path.isLocalFile()){
+
+    qDebug() << "Ouch! This is remote file. We don't have handling for that "
+                "right now";
+    return false;
+  }
+
+  QString qstr = path.toLocalFile();
+  QImage img{};
+
+  if (!img.load(qstr)) {
+    qDebug() << "Loaded file failed";
+    return false;
+  }
+
+  m_map = QPixmap::fromImage(std::move(img));
+
+  if (m_map.isNull()){
+    qDebug() << "Loaded file is null";
+    return false;
+  }
+
+ qDebug() << "File" << qstr << "loaded successfully";
+  update();
+  return true;
 }
 
-int LedRuler::lx() const
-{
-    return m_pix_rect.x();
-}
+void LedRuler::paint(QPainter *painter){
 
-int LedRuler::ly() const
-{
-    return m_pix_rect.y();
-}
-
-void LedRuler::setLx(const int &lx) 
-{
-     m_pix_rect.setX(lx);
-}
-
-void LedRuler::setLy(const int &ly) 
-{
-     m_pix_rect.setY(ly);
-}
-
-int LedRuler::rotation() const 
-{
-    return m_rotation;
-}
-
-void LedRuler::setRotation(const int &rotation)
-{
-    m_rotation = rotation;
-}
-
-QPixmap LedRuler::getPixMap() const
-{
-    return m_part_map;
-}
-
-bool LedRuler::setPixMap(QUrl path)
-{
-    
-    QString qstr = path.toEncoded();
-    if(!m_map.load(qstr) || m_map.isNull()) 
-    {
-        return false;
-    }
-
-    qDebug() << "File" << qstr << "loaded successfully";
-    update();
-    
-    return true;
-}
-
-void LedRuler::paint(QPainter *painter)
-{
     painter->drawPixmap(0, 0, m_map);
     painter->rotate(m_rotation);
     painter->setBrush(m_color);
@@ -90,19 +70,13 @@ void LedRuler::paint(QPainter *painter)
     qDebug() << "XXXXXXX";
 }
 
+void LedRuler::rulerUpdate(int x,int y){
 
-void LedRuler::rulerUpdate(int x,int y)
-{
     m_pix_rect.moveTo(x - m_pix_rect.height()/2, y - m_pix_rect.height()/2);
     m_part_map = m_map.copy(m_pix_rect);
 
     Interpolation::setLedColor(this);
-    
-    update();
-    
+    update(); 
 }
 
-void LedRuler::rulerUpdate()
-{
-    update();
-}
+void LedRuler::rulerUpdate(){ update(); }
--- BasicGUI/LedRuler.h
+++ BasicGUI/LedRuler.h
@@ -2,25 +2,24 @@
 #ifndef LED_MATRIX_H
 #define LED_MATRIX_H
 
+#include <QColor>
 #include <QString>
 #include <QObject>
 #include <QtQuick/QQuickPaintedItem>
-#include <QColor>
 #include <QPainter>
 #include <QPixmap>
-#include <iostream>
+#include <QUrl>
 #include <QDebug>
+#include <iostream>
 
+class LedRuler : public QQuickPaintedItem{
 
-class LedRuler : public QQuickPaintedItem
-{
     Q_OBJECT
     Q_PROPERTY(QColor color READ color WRITE setColor)
     Q_PROPERTY(int size READ size WRITE setSize)
     Q_PROPERTY(int lx READ lx WRITE setLx)
     Q_PROPERTY(int ly READ ly WRITE setLy)
     Q_PROPERTY(int rotation READ rotation WRITE setRotation)
-
     QML_ELEMENT
 
 public:
@@ -38,13 +37,11 @@ public:
     int ly() const;
     void setLx(const int &lx);
     void setLy(const int &ly);
-
     int rotation() const;
     void setRotation(const int &rotation);
 
     QPixmap getPixMap() const;
     
-
  Q_INVOKABLE void rulerUpdate(int l_x, int l_y);
  Q_INVOKABLE void rulerUpdate();
  Q_INVOKABLE bool setPixMap(QUrl path);
@@ -53,15 +50,11 @@ signals:
     void rulerUpdateSignal();
 
 private:
-    
     QRect m_pix_rect;   //kwadrat reprezentujacu jeden pixel
     QPixmap m_map;         //wczytana bitmapa jak obraz
     QPixmap m_part_map;  //bitmapa pobrana z obrazka
-
     QColor m_color;     
-
-    int m_rotation;
-    
+    int m_rotation;   
 };
 #endif
 
