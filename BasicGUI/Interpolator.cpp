a504c87ad22e1532168268c4ed3e2fbaff278322
Mateusz Pleśniak <mplesniak@sii.pl>
2022-12-18 22:41:28 +0100

    refactoring Interpolator class 2st step

--- BasicGUI/Interpolator.cpp
+++ BasicGUI/Interpolator.cpp
@@ -14,36 +14,29 @@ QColor Interpolator::interpolatorSetLedColor(QVector<QPointF> vector_points)
     if (inerpolator_pixmap != nullptr)
         image = inerpolator_pixmap->toImage();
 
-    QColor color;
-
-    red_component.clearMembers();
-    green_component.clearMembers();
-    blue_component.clearMembers();
+    QColor led_color{
+        0,
+        0,
+        0,
+    };
 
     for (QPointF point : vector_points)
     {
-
-        color = image.pixelColor(static_cast<int>(point.x()),
-                                 static_cast<int>(point.y()));
-
-        red_component.sumUpdate(color.red());
-        green_component.sumUpdate(color.green());
-        blue_component.sumUpdate(color.blue());
+        increaseTotalInensivity(led_color,
+                                image.pixelColor(static_cast<int>(point.x()),
+                                                 static_cast<int>(point.y())));
     }
 
     int rect_size = vector_points.size();
 
     if (rect_size > 0)
     {
-        red_component.calculateColorIntensivity(rect_size);
-        green_component.calculateColorIntensivity(rect_size);
-        blue_component.calculateColorIntensivity(rect_size);
-
-        color = color.fromRgb(red_component.componentColorIntensivity(),
-                              green_component.componentColorIntensivity(),
-                              blue_component.componentColorIntensivity());
-
-        return color;
+        return led_color = calculateaAverageIntensivity(rect_size, led_color);
+    }
+    else
+    {
+        led_color.setRgb(0, 0, 0);
+        return led_color;
     }
 }
 //////////////////////////////////////////////////////////////////
@@ -70,3 +63,23 @@ QVector<QPointF> Interpolator::interpolatorTransform(Transform transform,
 }
 
 ///////////////////////////////////////////////////////////////////
+namespace
+{
+void increaseTotalInensivity(QColor& color, QColor color_from_image)
+{
+
+    color.setRedF(color.redF() + color_from_image.redF());
+    color.setGreenF(color.greenF() + color_from_image.greenF());
+    color.setBlueF(color.blueF() + color_from_image.blueF());
+}
+
+QColor calculateaAverageIntensivity(int sample_amount, QColor total_intensivity)
+{
+    QColor color;
+    color.setRedF(total_intensivity.redF() / sample_amount);
+    color.setGreenF(total_intensivity.greenF() / sample_amount);
+    color.setBlueF(total_intensivity.blueF() / sample_amount);
+
+    return color;
+}
+} // namespace
--- BasicGUI/Interpolator.h
+++ BasicGUI/Interpolator.h
@@ -13,34 +13,7 @@
  * @brief Instance of ColorColmponent Class represent single color component
  * in RGB image format.
  */
-//////////////////////////////////////////////////////////////////
-class ColorComponent
-{
 
-public:
-    ColorComponent() : sum(0), level(0) {}
-
-    void clearMembers()
-    {
-        int sum = 0;
-        int level = 0;
-    }
-
-    void sumUpdate(int color) { sum += color; }
-
-    void calculateColorIntensivity(int sample_amount)
-    {
-        level = sum / sample_amount;
-    }
-
-    int componentColorIntensivity() { return level; }
-
-private:
-    int sum;
-    int level;
-};
-
-//////////////////////////////////////////////////////////////////
 class Interpolator : public QObject
 {
     Q_OBJECT
@@ -51,10 +24,13 @@ public:
     QColor interpolatorSetLedColor(QVector<QPointF> vector_points);
     QVector<QPointF> interpolatorTransform(Transform transform, QRect rect);
     QPixmap* inerpolator_pixmap;
-
-private:
-    ColorComponent red_component;
-    ColorComponent green_component;
-    ColorComponent blue_component;
 };
+
+namespace
+{
+void increaseTotalInensivity(QColor& color, QColor color_from_image);
+QColor calculateaAverageIntensivity(int sample_amount,
+                                    QColor total_intensivity);
+} // namespace
+
 #endif
