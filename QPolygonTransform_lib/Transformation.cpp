481d08a8fe110efb8b0c33b8e5634f53bc3dd730
Mateusz Pleśniak <mplesniak@sii.pl>
2023-02-23 19:55:37 +0100

    simplify Tranform class

--- QPolygonTransform_lib/Transformation.cpp
+++ QPolygonTransform_lib/Transformation.cpp
@@ -6,44 +6,16 @@
 
 Transform::Transform(QPoint rotCenter, int deg_angle) : m_rotCenter(rotCenter)
 {
-    m_angle = convertAngleToRad(deg_angle);
+    m_angle = deg_angle * 3.14159 / 180;
 }
 
 QPointF Transform::operator()(QPointF point)
 {
-    float a_angle = calcAngle(point);
-    float a_module = calcModule(point, a_angle);
+    float a_angle = atan2(point.y(), point.x()); // calculation angle
+    float a_module = point.x() / cosf(a_angle);  // calculation module
     m_angle += a_angle;
-    QPointF a_point = transformPoint(m_rotCenter, a_module, m_angle);
 
+    QPointF a_point{a_module * cos(m_angle) + m_rotCenter.x(), // calc new point
+                    a_module * sin(m_angle) + m_rotCenter.y()};
     return a_point;
 }
-////////////////////////////////////////////////////////////////////////////
-namespace
-{
-
-float calcModule(QPointF point, float angle)
-{
-    float module = point.x() / cosf(angle);
-    return module;
-}
-
-float calcAngle(QPointF point)
-{
-    float angle = atan2(point.y(), point.x());
-    return angle;
-}
-
-float convertAngleToRad(int deg_angle)
-{
-    float ext_angle = deg_angle * 3.14159 / 180;
-    return ext_angle;
-}
-QPointF transformPoint(QPointF rotCenter, float module, float angle)
-{
-    QPointF i_point;
-    i_point.setX(module * cos(angle) + rotCenter.x());
-    i_point.setY(module * sin(angle) + rotCenter.y());
-    return i_point;
-}
-} // namespace
