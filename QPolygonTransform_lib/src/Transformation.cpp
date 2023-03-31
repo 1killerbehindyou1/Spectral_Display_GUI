a096e8a654234de2839b3bfc7bb74495cb5e4035
Mateusz Pleśniak <mplesniak@sii.pl>
2023-03-31 16:49:31 +0200

    corrected after review, defined pi as constexpr

--- QPolygonTransform_lib/src/Transformation.cpp
+++ QPolygonTransform_lib/src/Transformation.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 Transform::Transform(const QPointF& rotCenter, float deg_angle)
-    : m_rotCenter(rotCenter), m_angle(deg_angle * 3.14159 / 180)
+    : m_rotCenter(rotCenter), m_angle(degToRad(deg_angle))
 {
 }
 
@@ -17,10 +17,9 @@ QPointF Transform::operator()(const QPointF& point)
     std::complex<float> new_point_cpl(new_point.x(), new_point.y());
 
     float angle = std::arg(new_point_cpl);
-    if (new_point.y() < 0)
-    {
-        angle += 6.283;
-    }
+
+    angle += new_point.y() < 0 ? two_pi : 0;
+
     float module = std::abs(new_point_cpl);
     m_angle += angle;
     QPointF out_point{module * cos(m_angle), module * sin(m_angle)};
--- QPolygonTransform_lib/src/Transformation.h
+++ QPolygonTransform_lib/src/Transformation.h
@@ -8,6 +8,11 @@
 #include <QVector>
 #include <cmath>
 
+constexpr double pi = 3.141592653589793238462643383279502884197169399375L;
+constexpr double two_pi = 2 * pi;
+constexpr double inv_pi = 1 / pi;
+constexpr inline double degToRad(float deg) { return deg * pi / 180; }
+
 class Transform
 {
 public:
