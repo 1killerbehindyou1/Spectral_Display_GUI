0b5c69096f8f3d83cb1755afd8b82a40682557fb
Mateusz Pleśniak <mplesniak@sii.pl>
2023-05-07 16:42:21 +0200

    appy changes in BasicGui application - redesign rendering preview from generated previously QImage

--- BasicGUI/Src/LedRuler.cpp
+++ BasicGUI/Src/LedRuler.cpp
@@ -29,13 +29,19 @@ void LedRuler::paint(QPainter* painter)
         int lp_hight = m_current_transformed_image->height();
 
         int diff_angle = int(360 / lp_hight);
+<<<<<<< HEAD
         qDebug() << "width: " << lp_width; // << lp_hight;
         qDebug() << "diff_angle: " << diff_angle;
+=======
+        // qDebug() << "width: " << lp_width; // << lp_hight;
+        // qDebug() << "diff_angle: " << diff_angle;
+>>>>>>> 6c7c0d2 (appy changes in BasicGui application)
         qDebug() << *m_current_transformed_image;
 
         // painter->save();
         painter->translate(250, 250);
 
+<<<<<<< HEAD
         for (int curr_rot = 0; curr_rot < 360; curr_rot += diff_angle)
         {
             // painter->save();
@@ -66,6 +72,29 @@ void LedRuler::paint(QPainter* painter)
                         painter->setBrush(color);
                         painter->drawRect(current_led_rect);
                     }
+=======
+        for (int curr_rot = 0; curr_rot < lp_hight; curr_rot++)
+        {
+            painter->save();
+            painter->rotate(curr_rot * diff_angle);
+
+            // qDebug() << "curr_rot: " << curr_rot * diff_angle;
+            QRect current_led_rect{QPoint{0, 0}, QSize{m_size, m_size}};
+
+            for (int led_idx = 0; led_idx < lp_width; led_idx++)
+            {
+                current_led_rect.moveLeft(led_idx * m_size);
+
+                qDebug() << curr_rot << led_idx;
+                if (m_current_transformed_image->valid({curr_rot, led_idx}))
+                {
+                    QColor color{m_current_transformed_image->pixelColor(
+                        curr_rot, led_idx)};
+
+                    painter->setPen(Qt::transparent);
+                    painter->setBrush(color);
+                    painter->drawRect(current_led_rect);
+>>>>>>> 6c7c0d2 (appy changes in BasicGui application)
                 }
             }
             painter->restore();
