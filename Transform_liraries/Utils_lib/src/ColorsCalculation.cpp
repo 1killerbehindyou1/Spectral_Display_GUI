ae656cf48c09f837d9000f27b3b64788a2881561
Mateusz Pleśniak <mplesniak@sii.pl>
2023-06-26 14:57:08 +0200

    separate commonUtilsLib

--- Transform_liraries/Utils_lib/src/ColorsCalculation.cpp
+++ Transform_liraries/Utils_lib/src/ColorsCalculation.cpp
@@ -1,15 +1,4 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-namespace constants
-{
-
-constexpr double pi = 3.141592653589793238462643383279502884197169399375L;
-constexpr double two_pi = 2 * pi;
-constexpr double inv_pi = 1 / pi;
-constexpr inline double degToRad(float deg) { return deg * pi / 180; }
-
-} // namespace constants
+#include "../src/Common.hpp"
 
 namespace color
 {
@@ -35,5 +24,3 @@ QColor operator/(const QColor& total_intensivity, int sample_amount)
     return color;
 }
 } // namespace color
-
-#endif
