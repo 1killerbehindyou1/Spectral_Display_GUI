2abdfdfe30910718c85c28c044b0f6ffdba8022c
Mateusz Pleśniak <mplesniak@sii.pl>
2023-08-19 14:11:00 +0200

    move cmake path definition to one file

--- cmake_utils/install_gui.cmake
+++ cmake_utils/install_gui.cmake
@@ -1,4 +1,5 @@
 set(INSTALL_PATH "${CMAKE_SOURCE_DIR}/../1.Install_location")
+set(CMAKE_INSTALL_PREFIX "C:/Users/mplesniak/Desktop/1. Mentoring_QT_project/1.Install_location" CACHE PATH "" FORCE)
 
 install(TARGETS BasicGUI
     RUNTIME DESTINATION "${INSTALL_PATH}"
