a77500f07427cb068c16e2b4fe8c220b8fc70cdf
Mateusz Pleśniak <mplesniak@sii.pl>
2023-08-19 18:53:40 +0200

    small fixes

--- BasicGUI/Src/main.cpp
+++ BasicGUI/Src/main.cpp
@@ -1,84 +1,77 @@
-// #include "FileManager.hpp"
-// #include "InterpolatorQPoly.hpp"
-// #include "LedRuler.hpp"
-// #include <QGuiApplication>
-// #include <QQmlApplicationEngine>
-// #include <QQmlComponent>
-// #include <QQmlContext>
-// #include <QtQuick>
-// #include <thread>
+#include "FileManager.hpp"
+#include "InterpolatorQPoly.hpp"
+#include "LedRuler.hpp"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlComponent>
+#include <QQmlContext>
+#include <QtQuick>
+#include <thread>
 
-#include <iostream>
-// void myMessageOutput(QtMsgType type,
-//                      [[maybe_unused]] const QMessageLogContext& context,
-//                      const QString& msg)
-// {
-//     QByteArray localMsg = msg.toLocal8Bit();
-//     QString typeStr = "OTHER";
-//     switch (type)
-//     {
-//         case QtDebugMsg:
-//             typeStr = "DBG";
-//             break;
-//         case QtInfoMsg:
-//             typeStr = "INF";
-//             break;
-//         case QtWarningMsg:
-//             typeStr = "WRN";
-//             break;
-//         case QtCriticalMsg:
-//             typeStr = "CRT";
-//             break;
-//         case QtFatalMsg:
-//             typeStr = "FTL";
-//             break;
-//     }
-//     std::cout << QString("%1: %2")
-//                      .arg(typeStr)
-//                      .arg(localMsg.constData())
-//                      .toStdString()
-//               << std::endl;
-// }
+void myMessageOutput(QtMsgType type,
+                     [[maybe_unused]] const QMessageLogContext& context,
+                     const QString& msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    QString typeStr = "OTHER";
+    switch (type)
+    {
+        case QtDebugMsg:
+            typeStr = "DBG";
+            break;
+        case QtInfoMsg:
+            typeStr = "INF";
+            break;
+        case QtWarningMsg:
+            typeStr = "WRN";
+            break;
+        case QtCriticalMsg:
+            typeStr = "CRT";
+            break;
+        case QtFatalMsg:
+            typeStr = "FTL";
+            break;
+    }
+    std::cout << QString("%1: %2")
+                     .arg(typeStr)
+                     .arg(localMsg.constData())
+                     .toStdString()
+              << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
 
-    // #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    //     QCoreApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
-    // #endif
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+#endif
 
-    //     qInstallMessageHandler(myMessageOutput);
-    //     QGuiApplication app(argc, argv);
+    qInstallMessageHandler(myMessageOutput);
+    QGuiApplication app(argc, argv);
 
-    //     QQmlApplicationEngine engine;
-    //     const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
 
-    //     app.setOrganizationName("1killerbehindyou1");
-    //     app.setOrganizationDomain("Education");
+    app.setOrganizationName("1killerbehindyou1");
+    app.setOrganizationDomain("Education");
 
-    //     qmlRegisterType<LedRuler>("Main", 1, 0, "LedRuler");
-    //     qRegisterMetaType<QPixmap*>("QPixmap*");
-    //     qRegisterMetaType<QImage*>("QImage*");
+    qmlRegisterType<LedRuler>("Main", 1, 0, "LedRuler");
+    qRegisterMetaType<QPixmap*>("QPixmap*");
+    qRegisterMetaType<QImage*>("QImage*");
 
-    //     FileManager file_manager(&app);
+    FileManager file_manager(&app);
 
-    //     engine.rootContext()->setContextProperty("file_manager",
-    //     &file_manager);
+    engine.rootContext()->setContextProperty("file_manager", &file_manager);
 
-    //     QObject::connect(
-    //         &engine, &QQmlApplicationEngine::objectCreated, &app,
-    //         [url](QObject* obj, const QUrl& objUrl)
-    //         {
-    //             if (!obj && url == objUrl)
-    //                 QCoreApplication::exit(-1);
-    //         },
-    //         Qt::QueuedConnection);
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject* obj, const QUrl& objUrl)
+        {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
 
-    //     engine.load(url);
-    //     return app.exec();
-    while (1)
-    {
-        std::cout << "ggggggggg";
-    }
-    return 0;
+    engine.load(url);
+    return app.exec();
 }
