93e8aed4059c8501f41d2769ab46806794f521e6
Maciej Broda <nutek12@gmail.com>
2023-08-24 14:29:43 +0200

    Synchronisation with std::scoped_lock

--- threading_basics/main.cpp
+++ threading_basics/main.cpp
@@ -1,18 +1,26 @@
 #include <chrono>
 #include <functional>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <vector>
 
 using namespace std::chrono_literals;
 
+std::mutex cout_mx;
+
 void print_something(std::size_t count)
 {
     while (count-- > 0)
     {
-        std::cout << "[Thread " << std::this_thread::get_id()
-                  << "] Retries left : " << count << std::endl;
+        {
+
+            std::scoped_lock<std::mutex> _{cout_mx};
+            std::cout << "[Thread " << std::this_thread::get_id()
+                      << "] Retries left : " << count << std::endl;
+        }
+
         std::this_thread::sleep_for(100ms);
     };
 }
