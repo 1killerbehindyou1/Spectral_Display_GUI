f3e7d2a371281ae9ecaee024627cf5d5ed6aaef8
rzeźnik <you@example.com>
2026-03-24 15:26:48 +0100

    remove container from this PR

--- container/run_in_container.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-repo_root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-cd "${repo_root}"
-
-cmake --preset debug
-cmake --build --preset debug
-./build/debug/bin/BasicGUI
